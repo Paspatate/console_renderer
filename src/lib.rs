9eb06f408af9d17681714f55176660b661e2d94c
PasPatate <romigami@orange.fr>
2023-08-05 21:59:33 +0200

    added Screen struct and Drawable trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,28 @@
+use console::Term;
+use math::Vector2;
+
 pub mod math;
+
+#[allow(dead_code)]
+pub struct Screen {
+    title: String,
+    size: Vector2<i32>,
+    term: Term,
+    elements: Vec<Box<dyn Drawable>>,
+}
+
+
+impl Screen {
+    pub fn new(title: String, size: Vector2<i32>) -> Screen {
+        Screen {
+            title, 
+            size, 
+            term: Term::stdout(), 
+            elements: Vec::new(),
+        }
+    }
+}
+
+pub trait Drawable {
+    fn draw(&self, destination:Term);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use console_renderer::math;
+use console::Term;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>>{
     let test_vec2 = math::Vector2::new(1f64, 5f64);
     let target_vec2 = math::Vector2::new(2f64, 10f64);
     
@@ -8,4 +9,15 @@ fn main() {
     assert_eq!(test_vec2.x, target_vec2.x);
 
     println!("{:?}", test_vec2);
+    
+    println!("This is {} neat", console::style("quite").cyan());
+
+    let term: Term = Term::stdout();
+    term.clear_screen();
+    loop {
+        term.write_line(&format!("{:?}", term.size())[..])?;
+        term.move_cursor_to(0, 0)?;
+    }
+
+    Ok(())
 }
