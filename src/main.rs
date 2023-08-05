927182b028c7ee888e95a3d13c42e55f4d8eb7b0
PasPatate <romigami@orange.fr>
2023-08-05 19:47:14 +0200

    implemented Sub, Add, Mul trait

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use console_renderer::math;
 
 fn main() {
     let test_vec2 = math::Vector2::new(1f64, 5f64);
-    println!("{:?}", test_vec2);
+    let target_vec2 = math::Vector2::new(2f64, 10f64);
     
+    let test_vec2 = test_vec2 * 2f64;
+    assert_eq!(test_vec2.x, target_vec2.x);
+
+    println!("{:?}", test_vec2);
 }
--- src/math.rs
+++ src/math.rs
@@ -1,11 +1,47 @@
+use std::ops::{Add, Sub, Mul};
+
 #[derive(Debug)]
 pub struct Vector2<T> {
-    x: T,
-    y: T,
+    pub x: T,
+    pub y: T,
 }
 
 impl<T> Vector2<T> {
     pub fn new(x:T, y:T) -> Vector2<T> {
         Vector2 { x, y }
+    }
+}
+
+impl<T: Add<Output = T>> Add for Vector2<T>{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl<T: Sub<Output = T>> Sub for Vector2<T> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+
+impl<T> Mul<T> for Vector2<T> where T: Mul<Output = T> + Copy {
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        let other = &rhs;
+        Self {
+            x: self.x * *other,
+            y: self.y * *other,
+        }
     }
 }
